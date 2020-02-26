d3c9718b1fd65e0e242760c02cdd1085eda5c60b
alisherKak <alisher.alish333@gmail.com>
2020-02-26 22:28:18 +0600

    Add radom generation in certain radius

--- Assets/Scripts/RandomGenerator.cs
+++ Assets/Scripts/RandomGenerator.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RandomGenerator : MonoBehaviour
 {
+    [SerializeField]
+    private float radius;
+
+    [SerializeField]
+    private GameObject smallEnemy, meduimEnemy, largeEnemy, mine;
+
+    [SerializeField]
+    private float delayMinTime, delayMaxTime;
+
+    [SerializeField]
+    private Transform randomZone;
+
+    private float disposeTime;
+    private float delayTime;
+    private GameObject instantiatedObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        disposeTime = 0;
+        delayTime = Random.Range(delayMinTime, delayMaxTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(disposeTime <= 0)
+        {
+            delayTime -= Time.deltaTime;
+        }
+        else
+        {
+            disposeTime -= Time.deltaTime;
+        }
         
+        if(delayTime <= 0)
+        {
+            Destroy(instantiatedObject);
+
+            int instObject = Random.Range(1, 4);
+
+            Vector3 randomPosition = randomZone.position + Random.insideUnitSphere * radius;
+
+            switch(instObject)
+            {
+                case 1:
+                    instantiatedObject = Instantiate(smallEnemy, randomPosition, randomZone.rotation);
+                    break;
+                case 2:
+                    instantiatedObject = Instantiate(meduimEnemy, randomPosition, randomZone.rotation);
+                    break;
+                case 3:
+                    instantiatedObject = Instantiate(largeEnemy, randomPosition, randomZone.rotation);
+                    break;
+                case 4:
+                    instantiatedObject = Instantiate(mine, randomPosition, randomZone.rotation);
+                    break;
+            }
+
+            disposeTime = 5;
+            delayTime = Random.Range(delayMinTime, delayMaxTime);
+        }
     }
 }
